baef054a457a0ed569eb7be4981cb6e7d65fe168
Timo Salm <tsalm@pivotal.io>
2022-01-13 17:51:24 +0100

    Add TAS adapter 0.2.0 installation script

---
+++ tas-adapter/README.md
@@ -0,0 +1,62 @@
+# Unofficial Application Service Adapter for VMware Tanzu Application Platform (0.2.0)
+
+It's always recommended to go through the official documentation in addition to this guide!
+The scripts and commands in this guide were executed on a Amazon Linux 2 jumpbox. It's recommended to go through them step by step!
+
+## Resources
+ - [Public beta announcement](https://tanzu.vmware.com/content/blog/application-service-adapter-for-vmware-tanzu-application-platform-2)
+ - [0.2.0 documentation](https://docs.vmware.com/en/Application-Service-Adapter-for-VMware-Tanzu-Application-Platform/0.2/tas-adapter-0-2/GUID-overview.html)
+
+## Things additionally handled in the installation script
+- The 500M default app memory allocation is not sufficient for Java apps, and the app manifest does not yet accept the memory parameter to set it on push -> Set default to 1024M
+
+## Prerequisities
+- You have to create the following private projects in Harbor `tas-adapter-droplets`, `tas-adapter-packages`. For other registries you may have to change the format of the `kpack_image_tag_prefix` and `package_registry_base_path` configuration values in `tas-adapter-values.yaml` 
+
+## Installation
+Copy values-example.yaml to values.yaml and set configuration values
+```
+cp values-example.yaml values.yaml
+```
+
+Run the installation script.
+```
+./install.sh
+```
+
+## Known Issues / workarounds
+- If you further have to increase the app memory allocation and don't want to override the default values, you can run `cf scale APP-NAME -m 2G` after a `cf push APP-NAME`
+- If you have pushed an application with multiple processes, which is the case for most of e.g. the Spring Boot apps built by TBS, all commands related to apps fail with the following error:
+	```
+	Error unmarshalling the following into a cloud controller error: upstream connect error or disconnect/reset before headers. reset reason: connection termination
+	```
+	The workaround is to get all the processes via `curl /v3/processes | jq '.resources[] | {type, instances, relationships}'` and scale to 1 via `cf scale APP-NAME -i 1 --process PROCESS-TYPE`.
+	For a typical Spring Boot app the workaround should be:
+	```
+	cf scale APP-NAME -i 1 --process executable-jar
+	cf scale APP-NAME -i 1 --process task
+	```
+## Usage
+```
+cf api api-tas-adapter.<your-ingress-domain>
+cf login
+cf create-org my-org
+cf target -o "my-org"
+cf create-space my-space
+cf target -o "my-org" -s "my-space"
+
+git clone https://github.com/tsalm-pivotal/spring-boot-hello-world.git
+cd spring-boot-hello-world
+cf push hello-world
+
+# See section "Known Issues / workarounds"
+cf scale hello-world -i 1 --process executable-jar
+cf scale hello-world -i 1 --process task
+
+curl https://hello-world.tas-adapter.<your-ingress-domain> 
+
+cf apps
+cf routes
+cf orgs
+cf spaces
+```
---
+++ tas-adapter/install.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+mkdir -p generated
+
+
+ytt --ignore-unknown-comments -f values.yaml -f additonal-ingress-config/ | kubectl apply -f-
+
+sudo wget -O /etc/yum.repos.d/cloudfoundry-cli.repo https://packages.cloudfoundry.org/fedora/cloudfoundry-cli.repo
+sudo yum install cf8-cli
+cf version
+
+kubectl create ns tas-adapter-install
+
+export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
+export INSTALL_REGISTRY_USERNAME=$(cat values.yaml | grep tanzunet -A 3 | awk '/username:/ {print $2}')
+export INSTALL_REGISTRY_PASSWORD=$(cat values.yaml  | grep tanzunet -A 3 | awk '/password:/ {print $2}')
+tanzu secret registry add tap-registry \
+  --username ${INSTALL_REGISTRY_USERNAME} --password ${INSTALL_REGISTRY_PASSWORD} \
+  --server ${INSTALL_REGISTRY_HOSTNAME} \
+  --namespace tas-adapter-install
+
+tanzu package repository add tas-adapter-repository \
+  --url registry.tanzu.vmware.com/app-service-adapter/tas-adapter-package-repo:0.2.0 \
+  --namespace tas-adapter-install
+ytt -f tas-adapter-values.yaml -f values.yaml --ignore-unknown-comments > generated/tas-adapter-values.yaml
+tanzu package install tas-adapter \
+  --package-name application-service-adapter.tanzu.vmware.com \
+  --version 0.2.0 \
+  --values-file generated/tas-adapter-values.yaml \
+  --namespace tas-adapter-install
+
+INGRESS_TLS_SECRET_NAME=$(cat values.yaml | grep ingress -A 3 | awk '/contour_tls_secret:/ {print $2}')
+INGRESS_TLS_SECRET_NAMESPACE=$(cat values.yaml | grep ingress -A 3 | awk '/contour_tls_namespace:/ {print $2}')
+cp overlays/tas-adapter/ingress-overlay.yaml generated/ingress-overlay.yaml
+sed -i"" -e "s/INGRESS_TLS_SECRET_NAMESPACE/$INGRESS_TLS_SECRET_NAMESPACE/" generated/ingress-overlay.yaml
+sed -i"" -e "s/INGRESS_TLS_SECRET_NAME/$INGRESS_TLS_SECRET_NAME/" generated/ingress-overlay.yaml
+kubectl create secret generic ingress-secret-name-overlay --from-file=ingress-secret-name-overlay.yaml=generated/ingress-overlay.yaml -n tas-adapter-install
+kubectl annotate packageinstalls tas-adapter -n tas-adapter-install ext.packaging.carvel.dev/ytt-paths-from-secret-name.0=ingress-secret-name-overlay
+
+DEFAULT_APP_MEMORY_ALLOCATION=$(cat values.yaml  | grep default_app_memory_allocation | awk '/default_app_memory_allocation:/ {print $2}')
+cp overlays/tas-adapter/workload-configuration-overlay.yaml generated/workload-configuration-overlay.yaml
+sed -i"" -e "s/INGRESS_TLS_SECRET_NAMESPACE/$INGRESS_TLS_SECRET_NAMESPACE/" generated/workload-configuration-overlay.yaml
+sed -i"" -e "s/INGRESS_TLS_SECRET_NAME/$INGRESS_TLS_SECRET_NAME/" generated/workload-configuration-overlay.yaml
+sed -i"" -e "s/DEFAULT_APP_MEMORY_ALLOCATION/$DEFAULT_APP_MEMORY_ALLOCATION/" generated/workload-configuration-overlay.yaml
+kubectl create secret generic workload-configuration-overlay --from-file=workload-configuration-overlay.yaml=generated/workload-configuration-overlay.yaml -n tas-adapter-install
+kubectl annotate packageinstalls tas-adapter -n tas-adapter-install ext.packaging.carvel.dev/ytt-paths-from-secret-name.1=workload-configuration-overlay
+# Delete cf-k8s-controllers-controller-manager pod so that configuration changes take effect 
+DEFAULT_APP_MEMORY_ALLOCATION=$(cat values.yaml  | grep default_app_memory_allocation | awk '/default_app_memory_allocation:/ {print $2}')
+OVERRIDEN_CONFIG=$(kubectl get cm cf-k8s-controllers-config -n cf-k8s-controllers-system -o jsonpath='{.data}')
+until grep -q "$DEFAULT_APP_MEMORY_ALLOCATION" <<< "$OVERRIDEN_CONFIG";
+do
+  echo "Waiting until config override happend ..."
+  sleep 1
+  OVERRIDEN_CONFIG=$(kubectl get cm cf-k8s-controllers-config -n cf-k8s-controllers-system -o jsonpath='{.data}')
+done
+kubectl delete pods -l control-plane=controller-manager -n cf-k8s-controllers-system
+
+export CONTAINER_REGISTRY_HOSTNAME=$(cat values.yaml | grep container_registry -A 3 | awk '/hostname:/ {print $2}')
+export CONTAINER_REGISTRY_USERNAME=$(cat values.yaml | grep container_registry -A 3 | awk '/username:/ {print $2}')
+export CONTAINER_REGISTRY_PASSWORD=$(cat values.yaml | grep container_registry -A 3 | awk '/password:/ {print $2}')
+kubectl create secret docker-registry image-registry-credentials \
+  -n cf \
+  --docker-server=${CONTAINER_REGISTRY_HOSTNAME} \
+  --docker-username=${CONTAINER_REGISTRY_USERNAME} \
+  --docker-password=${CONTAINER_REGISTRY_PASSWORD}
