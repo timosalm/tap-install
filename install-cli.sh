278e008169e5b6ed6d32a9a2d14718bee3d32c51
Gorkem Ozlu <spammailatmayin@gmail.com>
2022-01-20 13:16:01 +0300

    add insight binary

--- install-cli.sh
+++ install-cli.sh
@@ -13,4 +13,8 @@ tanzu version
 
 tanzu plugin install --local cli all
 tanzu plugin list
+
+pivnet download-product-files --product-slug='supply-chain-security-tools' --release-version='v1.0.0-beta.4' --product-file-id=1130458
+chmod +x insight-1.0.1_darwin_amd64
+sudo install insight-1.0.1_darwin_amd64 /usr/local/bin/insight
 cd ..
