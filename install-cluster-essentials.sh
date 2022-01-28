0303109763b64edb39cc9247e993e0d33d5b5b72
odedia <odedia@gmail.com>
2022-01-28 11:07:39 +0200

    support for mac or linux

--- install-cluster-essentials.sh
+++ install-cluster-essentials.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-pivnet download-product-files --product-slug='tanzu-cluster-essentials' --release-version='1.0.0' --product-file-id=1105818
+platform="$1"
+
+case "${1:linux}" in
+	darwin) PRODUCT_ID=1105820;;
+	linux) PRODUCT_ID=1105818;;
+esac
+
+pivnet download-product-files --product-slug='tanzu-cluster-essentials' --release-version='1.0.0' --product-file-id=$PRODUCT_ID
 mkdir tanzu-cluster-essentials
-tar -xvf tanzu-cluster-essentials-linux-amd64-1.0.0.tgz -C tanzu-cluster-essentials
+
+tar -xvf "tanzu-cluster-essentials-$1-amd64-1.0.0.tgz" -C tanzu-cluster-essentials
 
 export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:82dfaf70656b54dcba0d4def85ccae1578ff27054e7533d08320244af7fb0343
 export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
