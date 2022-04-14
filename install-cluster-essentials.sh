2e774842bf2c310beee0a17f0e1230f844b39d76
Asim Shaikh <36833379+pivotal-asim-shaikh@users.noreply.github.com>
2022-04-14 13:07:53 -0400

    Cluster Essentials 1.1
    Update install bundle to pull Cluster Essentials 1.1 image

--- install-cluster-essentials.sh
+++ install-cluster-essentials.sh
@@ -10,7 +10,7 @@ mkdir tanzu-cluster-essentials
 
 tar -xvf "tanzu-cluster-essentials-$1-amd64-1.0.0.tgz" -C tanzu-cluster-essentials
 
-export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:82dfaf70656b54dcba0d4def85ccae1578ff27054e7533d08320244af7fb0343
+export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:ab0a3539da241a6ea59c75c0743e9058511d7c56312ea3906178ec0f3491f51d
 export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
 export INSTALL_REGISTRY_USERNAME=$(cat values.yaml | grep tanzunet -A 3 | awk '/username:/ {print $2}')
 export INSTALL_REGISTRY_PASSWORD=$(cat values.yaml  | grep tanzunet -A 3 | awk '/password:/ {print $2}')
